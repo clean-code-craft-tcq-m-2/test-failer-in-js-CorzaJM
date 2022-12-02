1a7f9b386b73f7c89085d05a99cd829819054486
Juan Manuel Corza Hermosillo <109678647+CorzaJM@users.noreply.github.com>
2022-12-02 08:16:24 -0600

    Adding aligne test case

--- misaligned.js
+++ misaligned.js
@@ -1,11 +1,14 @@
 const {expect} = require('chai')
 
+let colors = [];
+
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            colors.push(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`)
+            console.log(push[i]);
         }
     }
     return majorColors.length * minorColors.length;
@@ -13,4 +16,7 @@ function print_color_map() {
 
 result = print_color_map();
 expect(result).equals(25);
+for(let x = 1; x < colors.length(); x++){
+    expect(colors[x-1].length()).equals(colors[x].length());
+}
 console.log('All is well (maybe!)');
